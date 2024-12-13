eca5ab462f74fbb42c2978123b211cdb123e71c5
mikelatiolais <mike@latiolais.net>
2024-12-12 16:15:35 -0800

    Update bluetooth_conn.py

--- mpf/platforms/bluetooth_conn.py
+++ mpf/platforms/bluetooth_conn.py
@@ -1,11 +1,11 @@
-"""Contains the code for the bluetooth virtual switch platform
+"""Contains the code for the bluetooth virtual switch platform.
 
    This functionality is useful for local communication with secondary controllers and other pinball machines.
-   It presents 10 virtual switches
+   It presents 10 virtual switches.
 
 """
-import asyncio
-import logging
+//import asyncio
+//import logging
 
 from typing import Dict, List, Optional  # pylint: disable-msg=cyclic-import,unused-import
 
